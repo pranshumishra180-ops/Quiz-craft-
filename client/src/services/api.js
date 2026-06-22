9e0426c7aefa954eec1b64286d6711701ed1771b
Pranshu Mishra <pranshumishra180@gmail.com>
2026-06-22 16:41:44 +0530

    Update production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const api=axios.create({
-    baseURL: "http://localhost:3000/api",
-    withCredentials:true,
+const api = axios.create({
+  baseURL: "https://quiz-craft-backend-g539.onrender.com/api",
+  withCredentials: true,
 });
-export default api;
 
+export default api;
