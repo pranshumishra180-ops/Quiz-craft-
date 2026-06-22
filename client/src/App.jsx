395bc6b4611f7b13dc26bcedddfa0324caf8e5ab
Pranshu Mishra <pranshumishra180@gmail.com>
2026-06-22 17:06:36 +0530

    Fix root route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,84 +1,20 @@
-import { BrowserRouter, Routes, Route }
-from "react-router-dom";
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import QuizList from "./pages/QuizList";
-import QuizPage from "./pages/QuizPage";
-import Leaderboard from "./pages/Leaderboard";
-import ResultPage from "./pages/ResultPage";
-import UploadPdf from "./pages/UploadPdf";
 import Dashboard from "./pages/Dashboard";
-import MyResults from "./pages/MyResult";
-import ProtectedRoute from "./components/ProtectedRoute";
-import ManageUsers from "./pages/ManageUsers";
-import AdminRoute from "./components/AdminRoute";
-
+import QuizList from "./pages/QuizList";
 
 function App() {
   return (
     <BrowserRouter>
-
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
 
-        <Route path="/login"element={<Login />} />
-
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-
-        <Route  path="/quiz" element={  <ProtectedRoute>  <QuizList /> </ProtectedRoute>   } />
-
-        <Route
-          path="/quiz/:id"
-          element={<ProtectedRoute> <QuizPage /></ProtectedRoute> } />
-        <Route
-          path="/leaderboard"
-          element={
-            <ProtectedRoute>
-              <Leaderboard />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/result"
-          element={
-            <ProtectedRoute>
-              <ResultPage />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route path="/upload" element={<UploadPdf />}/>
-
-<Route path="/dashboard" element={<Dashboard />}/> 
-
-<Route
-  path="/my-results"
-  element={<MyResults />}
-/>
-
-
-
-<Route
-  path="/manage-users"
-  element={<ManageUsers />}
-/>
-
-<Route
-  path="/manage-users"
-  element={
-    <AdminRoute>
-      <ManageUsers />
-    </AdminRoute>
-  }
-/>
-
-
- </Routes>
-
-
-
-
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/quiz" element={<QuizList />} />
+      </Routes>
     </BrowserRouter>
   );
 }
