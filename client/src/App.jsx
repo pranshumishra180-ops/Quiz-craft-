ba8a1c34686e8cfa247ab119fa806f672d03dac2
Pranshu Mishra <pranshumishra180@gmail.com>
2026-06-22 18:34:43 +0530

    Fix React Router routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,82 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import QuizList from "./pages/QuizList";
+import Leaderboard from "./pages/Leaderboard";
+import ManageUsers from "./pages/ManageUsers";
+import UploadPDF from "./pages/UploadPDF";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
+        <Route
+          path="/"
+          element={<Navigate to="/login" />}
+        />
 
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/quiz" element={<QuizList />} />
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+
+        <Route
+          path="/register"
+          element={<Register />}
+        />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/quiz"
+          element={
+            <ProtectedRoute>
+              <QuizList />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/leaderboard"
+          element={
+            <ProtectedRoute>
+              <Leaderboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/manage-users"
+          element={
+            <ProtectedRoute>
+              <ManageUsers />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/upload-pdf"
+          element={
+            <ProtectedRoute>
+              <UploadPDF />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
